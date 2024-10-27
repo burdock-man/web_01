5093156fe51197a287df1d76b3a4f91878c43aa6
burdock-man <burdockman@naver.com>
2024-10-28 08:00:15 +0900

    241028.01

--- study/yonsei_backend/숙제/1025 써브웨이,국민건보 슬라이드/js/공부★★★★★.js
+++ study/yonsei_backend/숙제/1025 써브웨이,국민건보 슬라이드/js/공부★★★★★.js
@@ -7,12 +7,24 @@ $(function(){
     var sbCurr = 0;
 
     $(sbBtn).click(function () {
-        var sbDir = sbCurr > $(sbBtn).index(this) ? "100%" : "-100%";
-        sbImg.eq(sbCurr).stop().animate({"left":sbDir},1000);
-        sbCurr = $(sbBtn).index(this);
-        sbImg.eq(sbCurr).css("left",(parseInt(sbDir))*-1+"%").stop().animate({"left":"0%"},1000);
+
+        var sbShow = $(sbBtn).index(this);
+
         $(sbBtn).removeClass("on");
         $(this).addClass("on");
+
+        if (sbCurr > sbShow) {
+            $(sbImg[sbShow]).css("left","-100%");
+            $(sbImg[sbCurr]).stop().animate({"left":"100%"}, 1000);
+            $(sbImg[sbShow]).stop().animate({"left":"0%"}, 1000);
+        } else if (sbCurr < sbShow) {
+            $(sbImg[sbShow]).css("left","100%");
+            $(sbImg[sbCurr]).stop().animate({"left":"-100%"}, 1000);
+            $(sbImg[sbShow]).stop().animate({"left":"0%"}, 1000);
+        } else if (sbShow == sbCurr) return
+
+        sbCurr = sbShow;
+
     });
 
     function sbAuto(){
@@ -36,6 +48,19 @@ $(function(){
     });
 
 
+    // 간소화한 sbBtn 클릭 이벤트 코딩. 참고할것.
+    // $(sbBtn).click(function(){
+    //     var sbShow = $(sbBtn).index(this);
+    //     $(sbBtn).removeClass("on");
+    //     $(this).addClass("on");
+    //     if (sbCurr === sbShow) return;
+    //     var direction = sbShow > sbCurr ? "100%" : "-100%";
+    //     $(sbImg[sbShow]).css("left", direction).stop().animate({"left": "0%"}, 500);
+    //     $(sbImg[sbCurr]).stop().animate({"left": (direction === "100%" ? "-100%" : "100%")}, 500);
+    //     sbCurr = sbShow;
+    // });
+    
+
     // 국민건강보험 홍보 ============================================================================
 
     var npPrevBtn = $(".n_pr_ctrl .prevBtn");
@@ -46,17 +71,25 @@ $(function(){
     var npNum = 0;
 
     $(npNextBtn).click(function () {
-        $(npImg[npNum]).stop().animate({"left":"-100%"}, 500);
-        npNum = (npNum+1) % npImg.length;
-        $(npImg[npNum]).css("left", "100%").stop().animate({"left":"0%"}, 500);
-        $(npTxt).text(npNum+1);
+
+        $(npImg[npNum]).stop().animate({"left":"-100%"}, 500); // 우선 커렌트 이미지 왼쪽으로 뺴고
+
+        npNum = (npNum+1) % npImg.length; // 커렌트에 +1값 붙여서 다음 인덱스가 올 준비. (%가 들어간 이유는 4가 될 때 커렌트값이 0이됨.)
+        $(npImg[npNum]).css("left", "100%").stop().animate({"left":"0%"}, 500); // 앞에 올 사진을 오른쪽에 붙이고 보여줌.
+    
+        $(npTxt).text(npNum+1); // 콘크롤박스에 현재 사진 기입.
+
     });
 
     $(npPrevBtn).click(function () {
+
         $(npImg[npNum]).stop().animate({"left":"100%"}, 500);
-        npNum = (npNum - 1 + npImg.length) % npImg.length;
+
+        npNum = (npNum - 1 + npImg.length) % npImg.length; // -1이 될 때 3이 나오는 계산식이 성립하면 됨. (인덱스번호는 0,1,2,3이니까.)
         $(npImg[npNum]).css("left", "-100%").stop().animate({"left":"0%"}, 500);
-            $(npTxt).text(npNum+1);
+    
+        $(npTxt).text(npNum+1);
+
     });
 
     function npAuto () {
@@ -76,7 +109,26 @@ $(function(){
     });
 
 
+
     // 서브웨이 사이드 ============================================================================
+
+    // var ssImg = $(".s_side_img li");
+    // var ssBtn = $(".s_side_ctrl li");
+    // var ssCurr = 0;
+
+    // $(ssBtn).click(function () {
+    //     var ssDir = ssCurr > $(ssBtn).index(this) ? "100%" : "-100%";
+    //     $(ssImg[ssCurr]).stop().animate({"left":ssDir},1000);
+    //     ssCurr = $(ssBtn).index(this);
+    //     // $(ssImg[ssCurr]).css("left",-ssDir).stop().animate({"left":"0%"},1000);
+    //     // 이걸 써봤는데 도저히 안되는 이유가 -ssDir때문이었음. -(-100%)면 당연히 100%일줄 알았는데 프로그래밍에는 그런 논리가 안먹힘.
+    //     // 아래와 같이 parseInt로 100만 추출 한 뒤, -1을 곱해주고 스트링"%"를 써주면 된다.
+    //     $(ssImg[ssCurr]).css("left", parseInt(ssDir)*-1+"%").stop().animate({"left":"0%"},1000);
+    //     $(ssBtn).removeClass("on");
+    //     $(this).addClass("on");
+    // });
+
+    //     이런저런 시도를 했고 결국 성공했으니 레퍼런스 없이 다시 해보자.
 
     var ssBtn = $(".s_side_ctrl li");
     var ssImg = $(".s_side_img li");
@@ -84,10 +136,10 @@ $(function(){
 
     ssBtn.click(function () {
         var ssDir = ssCurr > $(ssBtn).index(this) ? "100%" : "-100%";
-        ssImg.eq(ssCurr).stop().animate({"left":ssDir},1000);
+        ssImg.eq(ssCurr).stop().animate({"left":ssDir},1000); // 대괄호로 인덱스번호를 지정해주는 경우가 아니면 변수로 선언된 쿼리는 호출할 때 &() 안써도 된다.
         ssCurr = $(ssBtn).index(this);
         $(ssImg[ssCurr]).css("left",(parseInt(ssDir))*-1+"%").stop().animate({"left":"0%"},1000);
-        ssBtn.removeClass("on");
+        ssBtn.removeClass("on"); // 위에처럼 대괄호로 인덱스번호 지정할땐 변수에 &() 써야함.
         $(this).addClass("on");
     });
 
@@ -119,6 +171,7 @@ $(function(){
     var nbPrevBtn = $(".n_ban_ctrl .prevBtn");
     var nbStopBtn = $(".n_ban_ctrl .stopBtn");
     var nbNextBtn = $(".n_ban_ctrl .nextBtn");
+    var nbMoreBtn = $(".n_ban_ctrl .moreBtn");
     var nbTxt = $(".n_ban_ctrl .currNum");
     var nbNum = 0;
 
