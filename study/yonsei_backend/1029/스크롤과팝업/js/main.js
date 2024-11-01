1a47a7dd4660654b09151ac0b4debfe7b019fdd7
burdock-man <burdockman@naver.com>
2024-11-01 17:35:33 +0900

    241101.01

--- study/yonsei_backend/1029/스크롤과팝업/js/main.js
+++ study/yonsei_backend/1029/스크롤과팝업/js/main.js
@@ -109,22 +109,38 @@ $(function() {
         return false
     });
 
-    $(".main").mousewheel(function(event, d) {
-        // event 마우스 휠의 정보
-        // d 이동 방향
-        // console.log(d); // 마우스 휠을 위로 스크롤하면 양수(+1) 아래는 음수 (-1)
+    // $(".main").mousewheel(function(event, d) {
+    //     // event 마우스 휠의 정보
+    //     // d 이동 방향
+    //     // console.log(d); // 마우스 휠을 위로 스크롤하면 양수(+1) 아래는 음수 (-1)
 
+    //     if(d > 0) { // 휠 위로
+    //         var prev = $(this).prev().offset().top;
+    //         $("html").stop().animate({scrollTop : prev}, 1000, "easeOutBounce");
+    //     }
+    //     if(d < 0) { // 휠 아래로
+    //         var next = $(this).next().offset().top;
+    //         $("html").stop().animate({scrollTop : next}, 1000, "easeOutBounce");
+    //     }
 
+    // });
+
+    $("html").mousewheel(function(event, d) {
+        // 현재 스크롤 위치에서 가장 가까운 .main 요소를 찾음
+        var currentSection = $(".main").filter(function() {
+            return $(window).scrollTop() < $(this).offset().top + $(this).outerHeight();
+        }).first();
+        
         if(d > 0) { // 휠 위로
-            var prev = $(this).prev().offset().top;
-            $("html, bod").stop().animate({scrollTop : prev}, 1000, "easeOutBounce");
+            var prev = currentSection.prev().offset().top;
+            $("html").stop().animate({scrollTop: prev}, 1000, "easeOutBounce");
         }
         if(d < 0) { // 휠 아래로
-            var next = $(this).next().offset().top;
-            $("html, bod").stop().animate({scrollTop : next}, 1000, "easeOutBounce");
+            var next = currentSection.next().offset().top;
+            $("html").stop().animate({scrollTop: next}, 1000, "easeOutBounce");
         }
-
     });
+
 
     $("#popup").draggable();
     $("#notice_wrap").draggable();
@@ -160,5 +176,12 @@ $(function() {
         }
         $("#notice_wrap").fadeOut("fast");
     }
+
+    
+    $("#nav, #popup, #notice_wrap").mousewheel(function(event) {
+        event.preventDefault();  // 기본 스크롤 동작 차단
+        event.stopPropagation(); // 상위 요소로 이벤트 전파 중단
+    });
+    
 
 });
